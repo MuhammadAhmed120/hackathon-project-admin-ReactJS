8a3f114f7cfcb1e7c46ca53bb6655e0a6bd2338d
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2023-12-18 23:14:46 +0500

    Update login-view.jsx

--- src/sections/login/login-view.jsx
+++ src/sections/login/login-view.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Box from '@mui/material/Box';
@@ -11,8 +12,6 @@ import LoadingButton from '@mui/lab/LoadingButton';
 import { alpha, useTheme } from '@mui/material/styles';
 import InputAdornment from '@mui/material/InputAdornment';
 
-// import { useRouter } from 'src/routes/hooks';
-
 import { bgGradient } from 'src/theme/css';
 
 import Iconify from 'src/components/iconify';
@@ -24,22 +23,88 @@ export default function LoginView() {
 
   const navigate = useNavigate()
 
+  const [loading, setLoading] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const [validateError, setValidateError] = useState('')
+
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleClick = () => {
-    
-    navigate('/dashboard');
+  useEffect(() => {
+    localStorage.removeItem('token')
+  })
+
+  const validateEmail = () => {
+    const isValid = /\S+@\S+\.\S+/.test(email);
+    setEmailError(!isValid);
+    return isValid;
   };
+
+  const validatePassword = () => {
+    const isValid = password.length >= 6;
+    setPasswordError(!isValid);
+    return isValid;
+  };
+
+  const handleClick = async () => {
+    const isEmailValid = validateEmail();
+    const isPasswordValid = validatePassword();
+
+    if (isEmailValid && isPasswordValid) {
+      try {
+        // Make a POST request using Axios
+        const response = await axios.post(
+          'http://localhost:3002/login/admin',
+          {
+            panelEmail: email,
+            panelPassword: password,
+          }
+        );
+
+        console.log('Response:', response);
+        setValidateError('')
+        setLoading(true)
+
+        localStorage.setItem('token', response.data.token)
+
+        const loginToken = localStorage.getItem('token')
+        if (loginToken) {
+          console.log("YES")
+          navigate('/user');
+        }
+      } catch (error) {
+        // Handle error scenarios here
+        setValidateError(error.response.data.message)
+        console.error('Error:', error);
+      }
+    }
+  };
+
 
   const renderForm = (
     <>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
+        <TextField
+          name="email"
+          label="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          error={emailError}
+          helperText={emailError ? 'Please enter a valid email' : ''}
+        />
 
         <TextField
           name="password"
           label="Password"
           type={showPassword ? 'text' : 'password'}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          error={passwordError}
+          helperText={passwordError ? 'Password must be at least 6 characters' : ''}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -52,6 +117,10 @@ export default function LoginView() {
         />
       </Stack>
 
+      <Stack sx={{ margin: '20px 0', color: 'red' }}>
+        {validateError}
+      </Stack>
+
       <LoadingButton
         fullWidth
         size="large"
@@ -59,7 +128,7 @@ export default function LoginView() {
         variant="contained"
         color="inherit"
         onClick={handleClick}
-        sx={{ marginTop: 5 }}
+        loading={loading}
       >
         Login
       </LoadingButton>
