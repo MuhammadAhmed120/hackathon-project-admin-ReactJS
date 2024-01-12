70ea64e74b4cb59b855bec7e5e4be0bcfca1fa2a
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2024-01-13 01:34:20 +0500

    Update config-navigation.jsx

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -6,37 +6,24 @@ const icon = (name) => (
   <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
 );
 
+const token = localStorage.getItem('token')
+
 const navConfig = [
+  {
+    title: 'students',
+    path: '/user',
+    icon: icon('ic_user'),
+  },
   {
     title: 'attendance',
     path: '/',
     icon: icon('ic_analytics'),
   },
   {
-    title: 'students',
-    path: '/user',
-    icon: icon('ic_user'),
-  },
-  // {
-  //   title: 'product',
-  //   path: '/products',
-  //   icon: icon('ic_cart'),
-  // },
-  // {
-  //   title: 'blog',
-  //   path: '/blog',
-  //   icon: icon('ic_blog'),
-  // },
-  {
-    title: 'login',
+    title: token ? 'logout' : 'login',
     path: '/login',
     icon: icon('ic_lock'),
   },
-  // {
-  //   title: 'Not found',
-  //   path: '/404',
-  //   icon: icon('ic_disabled'),
-  // },
 ];
 
 export default navConfig;
