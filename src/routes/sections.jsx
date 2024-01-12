238a60b3c463ed0ba73d5cf664933e9ab6ecbb19
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2024-01-13 01:35:08 +0500

    Update sections.jsx

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -1,4 +1,5 @@
-import { lazy, Suspense } from 'react';
+import axios from 'axios';
+import { lazy, Suspense, useState, useEffect } from 'react';
 import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 
 import DashboardLayout from 'src/layouts/dashboard';
@@ -11,14 +12,41 @@ export const Page404 = lazy(() => import('src/pages/page-not-found'));
 // ----------------------------------------------------------------------
 
 export default function Router() {
+  const [tokenVerified, setTokenVerified] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    const validateToken = async () => {
+      try {
+        const response = await axios.post('http://localhost:3002/tokenverify', null, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+
+        console.log('response', response)
+        if (response.status === 200) {
+          return setTokenVerified(true)
+        }
+        return setTokenVerified(false)
+      } catch (error) {
+        console.log('error ', error)
+        return setTokenVerified(false)
+      }
+    }
+    validateToken()
+  }, [])
+
   const routes = useRoutes([
     {
-      element: (
+      element: tokenVerified ? (
         <DashboardLayout>
           <Suspense>
             <Outlet />
           </Suspense>
         </DashboardLayout>
+      ) : (
+        <LoginPage />
       ),
       children: [
         { element: <IndexPage />, index: true },
